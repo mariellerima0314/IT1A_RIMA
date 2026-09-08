2399bb9acb0089657baf523ce475299e03f2981f
Marielle Rima <marielle.rima@gmail.com>
2026-09-08 11:03:25 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,10 @@
+username = 'capisces'
+password = 'ceszrielle0103'
+
+u = input("Enter Username ---->    ")
+p = input("Enter Password ---->    ")
+
+if username == u and password == p :
+	print("ACCESS GRANTED")
+else:
+	print("ACCESS DENIED") 
